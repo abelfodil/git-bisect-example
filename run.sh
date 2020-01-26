30c6b7b8613945fdda0d62267db857591c12ad43
Anes Belfodil <ans.belfodil@gmail.com>
2020-01-26 18:36:06 -0500

    Add script to run program

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+BUILD_DIR="$SOURCE_DIR/build"
+
+NINJA=$([ -x "$(command -v ninja)" ] && echo "-GNinja")
+cmake -H"$SOURCE_DIR" -B"$BUILD_DIR" -DCMAKE_BUILD_TYPE=Release $NINJA && cmake --build "$BUILD_DIR" --target Doubler
+
+./build/Doubler
