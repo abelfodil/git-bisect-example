0229c31d116f7142a282aaf7f32b897914eb6abf
Anes Belfodil <ans.belfodil@gmail.com>
2020-01-26 18:28:13 -0500

    Double amount

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
+double double_amount(double amount) {
+    return amount * 2;
+}
+
 int main() {
     std::cout << "Entrez un montant à doubler: ";
     double amount{};
     std::cin >> amount;
-    
+
+    std::cout << double_amount(amount) << std::endl;
+
     return 0;
 }
